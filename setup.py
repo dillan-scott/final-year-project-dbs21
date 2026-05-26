563e20ae64e208db34b5b68fe4c083ad7db09fa9
Dillan Scott <dillan.scott22@imperial.ac.uk>
2026-05-26 14:17:29 +0100

    Update setup stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
 from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="final-year-project-dbs21",
     version="0.1",
     packages=find_packages(),
+    install_requires=requirements,
 )
